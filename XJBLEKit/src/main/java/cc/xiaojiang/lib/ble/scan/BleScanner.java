402fa72fe993135c2ce911911abdb722639c4010
Fxg950708 <1978640054@qq.com>
2022-03-11 13:38:08 +0800

    XJBLEKit-优化rn转蓝牙实体类 jitpack---0.1.2

--- XJBLEKit/src/main/java/cc/xiaojiang/lib/ble/scan/BleScanner.java
+++ XJBLEKit/src/main/java/cc/xiaojiang/lib/ble/scan/BleScanner.java
@@ -117,35 +117,36 @@ public class BleScanner {
         String platform = "";
         byte[] manufacturerSpecificData = null;
         XJBleDevice xjBleDevice = new XJBleDevice();
-        try {
-            ScanRecordUtil scanRecordUtil = ScanRecordUtil.parseFromBytes(scanRecordBytes);
-            if (scanRecordUtil.getManufacturerSpecificData(Constants.XJ_MANUFACTURER_ID) != null) {
-                manufacturerSpecificData = scanRecordUtil.getManufacturerSpecificData(XJ_MANUFACTURER_ID);
-                platform = XJBleDevice.PLATFORM_XJ;
-                xjBleDevice.setPlatform(XJBleDevice.PLATFORM_XJ);
-            } else if (scanRecordUtil.getManufacturerSpecificData(AL_MANUFACTURER_ID) != null) {
-                manufacturerSpecificData = scanRecordUtil.getManufacturerSpecificData(Constants.AL_MANUFACTURER_ID);
-                platform = XJBleDevice.PLATFORM_AL;
-                xjBleDevice.setPlatform(XJBleDevice.PLATFORM_AL);
-            }
-            if (manufacturerSpecificData == null) {
-                BleLog.w("get manufacturerSpecificData null!");
-                return null;
-            }
-            if (manufacturerSpecificData.length != 12) {
-                BleLog.w("manufacturerSpecificData length error= " + manufacturerSpecificData.length);
-                return null;
-            }
-            BleLog.d("get Manufacturer Specific Data: " + ByteUtils.bytesToHexString
-                    (manufacturerSpecificData));
-            ManufacturerData manufacturerData =
-                    new ManufacturerData(manufacturerSpecificData, platform);
-            xjBleDevice.setDevice(device);
-            xjBleDevice.setRssi(rssi);
-            xjBleDevice.setManufacturerData(manufacturerData);
-        } catch (Exception e) {
-            e.printStackTrace();
+        ScanRecordUtil scanRecordUtil = ScanRecordUtil.parseFromBytes(scanRecordBytes);
+
+        if (scanRecordUtil.getManufacturerSpecificData() == null) {
+            BleLog.w("get manufacturerSpecificData null!");
+            return null;
         }
+        if (scanRecordUtil.getManufacturerSpecificData(Constants.XJ_MANUFACTURER_ID) != null) {
+            manufacturerSpecificData = scanRecordUtil.getManufacturerSpecificData(XJ_MANUFACTURER_ID);
+            platform = XJBleDevice.PLATFORM_XJ;
+            xjBleDevice.setPlatform(XJBleDevice.PLATFORM_XJ);
+        } else if (scanRecordUtil.getManufacturerSpecificData(AL_MANUFACTURER_ID) != null) {
+            manufacturerSpecificData = scanRecordUtil.getManufacturerSpecificData(Constants.AL_MANUFACTURER_ID);
+            platform = XJBleDevice.PLATFORM_AL;
+            xjBleDevice.setPlatform(XJBleDevice.PLATFORM_AL);
+        }
+        if (manufacturerSpecificData == null) {
+            BleLog.w("get manufacturerSpecificData null!");
+            return null;
+        }
+        if (manufacturerSpecificData.length != 12) {
+            BleLog.w("manufacturerSpecificData length error= " + manufacturerSpecificData.length);
+            return null;
+        }
+        BleLog.d("get Manufacturer Specific Data: " + ByteUtils.bytesToHexString
+                (manufacturerSpecificData));
+        ManufacturerData manufacturerData =
+                new ManufacturerData(manufacturerSpecificData, platform);
+        xjBleDevice.setDevice(device);
+        xjBleDevice.setRssi(rssi);
+        xjBleDevice.setManufacturerData(manufacturerData);
         return xjBleDevice;
     }
 
