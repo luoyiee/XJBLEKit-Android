198aa0c334f3298fcf1a0a638d5d9bb19effb1da
Fxg950708 <1978640054@qq.com>
2022-03-16 14:25:56 +0800

    XJBLEKit-优化蓝牙连接 jitpack---0.3.7

--- XJBLEKit/src/main/java/cc/xiaojiang/lib/ble/scan/BleScanner.java
+++ XJBLEKit/src/main/java/cc/xiaojiang/lib/ble/scan/BleScanner.java
@@ -2,7 +2,12 @@ package cc.xiaojiang.lib.ble.scan;
 
 import static cc.xiaojiang.lib.ble.Constants.AL_MANUFACTURER_ID;
 import static cc.xiaojiang.lib.ble.Constants.XJ_MANUFACTURER_ID;
+import static cc.xiaojiang.lib.ble.utils.DataUtils.getBool;
+import static cc.xiaojiang.lib.ble.utils.DataUtils.getInt;
+import static cc.xiaojiang.lib.ble.utils.DataUtils.getString;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
@@ -12,15 +17,15 @@ import android.bluetooth.le.ScanSettings;
 import android.os.Looper;
 import android.os.ParcelUuid;
 import android.text.TextUtils;
-import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import cc.xiaojiang.lib.ble.XJBleDevice;
 import cc.xiaojiang.lib.ble.Constants;
 import cc.xiaojiang.lib.ble.IBleAuth;
-import cc.xiaojiang.lib.ble.XJBleDevice;
 import cc.xiaojiang.lib.ble.XJBleManager;
 import cc.xiaojiang.lib.ble.callback.BleConnectCallback;
 import cc.xiaojiang.lib.ble.callback.IBleScanCallback;
@@ -34,55 +39,44 @@ import cc.xiaojiang.lib.ble.utils.ByteUtils;
  * Created by facexxyz on 3/10/21.
  */
 public class BleScanner {
-
-    public static BleScanner getInstance() {
-        return ourInstance;
-    }
-
     private BleScanState mBleScanState = BleScanState.STATE_IDLE;
-
     private BluetoothLeScanner bluetoothLeScanner;
     private IBleScanCallback iBleScanCallback;
     private String autoConnectMac = "";
+    private String random = "";
     public Map<Object, Object> productMap;
     private ScanCallback scanCallback = new ScanCallback() {
         @Override
         public void onScanResult(int callbackType, ScanResult result) {
             super.onScanResult(callbackType, result);
-            XJBleDevice bleDevice = scanResultToBleDevice(result);
-            if (bleDevice == null) {
+            XJBleDevice XJBleDevice = scanResultToBleDevice(result);
+            if (XJBleDevice == null) {
                 return;
             }
-            BleLog.d(bleDevice.toString());
-            if (iBleScanCallback != null) {
-                iBleScanCallback.onLeDeviceScanned(scanResultToBleDevice(result));
-            }
+            BleLog.d(XJBleDevice.toString());
+            iBleScanCallback.onLeDeviceScanned(scanResultToBleDevice(result));
             if (TextUtils.isEmpty(autoConnectMac)) {
                 return;
             }
             //stop scan and connect
-//            stopScan();
+            stopScan();
         }
 
         @Override
         public void onBatchScanResults(List<ScanResult> results) {
             super.onBatchScanResults(results);
-            List<XJBleDevice> bleDeviceList = new ArrayList<>();
+            List<XJBleDevice> XJBleDeviceList = new ArrayList<>();
             for (ScanResult result : results) {
-                XJBleDevice bleDevice = scanResultToBleDevice(result);
-                bleDeviceList.add(bleDevice);
+                XJBleDevice XJBleDevice = scanResultToBleDevice(result);
+                XJBleDeviceList.add(XJBleDevice);
             }
-            if (iBleScanCallback != null) {
-                iBleScanCallback.onScanFinished(bleDeviceList);
-            }
+            iBleScanCallback.onScanFinished(XJBleDeviceList);
         }
 
         @Override
         public void onScanFailed(int errorCode) {
             super.onScanFailed(errorCode);
-            if (iBleScanCallback != null) {
-                iBleScanCallback.onScanFailed(errorCode);
-            }
+            iBleScanCallback.onScanFailed(errorCode);
         }
     };
 
@@ -92,17 +86,16 @@ public class BleScanner {
             BleLog.w("getScanRecord null!");
             return null;
         }
-        XJBleDevice bleDevice = new XJBleDevice();
+        XJBleDevice xjBleDevice = new XJBleDevice();
         byte[] manufacturerSpecificData = null;
-        String platform = "";
-        if (result.getScanRecord().getManufacturerSpecificData(Constants.XJ_MANUFACTURER_ID) != null) {
-            manufacturerSpecificData = result.getScanRecord().getManufacturerSpecificData(XJ_MANUFACTURER_ID);
-            platform = XJBleDevice.PLATFORM_XJ;
-            bleDevice.setPlatform(XJBleDevice.PLATFORM_XJ);
-        } else if (result.getScanRecord().getManufacturerSpecificData(AL_MANUFACTURER_ID) != null) {
-            manufacturerSpecificData = result.getScanRecord().getManufacturerSpecificData(Constants.AL_MANUFACTURER_ID);
-            platform = XJBleDevice.PLATFORM_AL;
-            bleDevice.setPlatform(XJBleDevice.PLATFORM_AL);
+        if (result.getScanRecord().getServiceUuids().contains(ParcelUuid.fromString(Constants.UUID_XJ_SERVICE))) {
+            manufacturerSpecificData =
+                    result.getScanRecord().getManufacturerSpecificData(Constants.XJ_MANUFACTURER_ID);
+            xjBleDevice.setPlatform(XJBleDevice.PLATFORM_XJ);
+        } else if (result.getScanRecord().getServiceUuids().contains(ParcelUuid.fromString(Constants.UUID_AL_SERVICE))) {
+            manufacturerSpecificData =
+                    result.getScanRecord().getManufacturerSpecificData(Constants.AL_MANUFACTURER_ID);
+            xjBleDevice.setPlatform(XJBleDevice.PLATFORM_AL);
         }
         if (manufacturerSpecificData == null) {
             BleLog.w("get manufacturerSpecificData null!");
@@ -115,29 +108,75 @@ public class BleScanner {
         BleLog.d("get Manufacturer Specific Data: " + ByteUtils.bytesToHexString
                 (manufacturerSpecificData));
         ManufacturerData manufacturerData =
+                new ManufacturerData(manufacturerSpecificData, xjBleDevice.getPlatform());
+        xjBleDevice.setDevice(result.getDevice());
+        xjBleDevice.setRssi(result.getRssi());
+        xjBleDevice.setManufacturerData(manufacturerData);
+        return xjBleDevice;
+    }
+
+    public XJBleDevice rnToBleDevice(HashMap<String, Object> propsMap, byte[] scanRecordBytes) {
+        XJBleDevice xjBleDevice = new XJBleDevice();
+        //获取厂商自定义格式广播
+        if (scanRecordBytes == null) {
+            return xjBleDevice;
+        }
+        String id = String.valueOf(propsMap.get("id"));
+        int rssi = Double.valueOf(String.valueOf(propsMap.get("rssi"))).intValue();
+        BluetoothDevice device = BluetoothAdapter.getDefaultAdapter().getRemoteDevice(id);
+        xjBleDevice.setDevice(device);
+        xjBleDevice.setRssi(rssi);
+        xjBleDevice.setId(id);
+        String platform = "";
+        byte[] manufacturerSpecificData = null;
+        ScanRecordUtil scanRecordUtil = ScanRecordUtil.parseFromBytes(scanRecordBytes);
+        if (scanRecordUtil.getManufacturerSpecificData() == null) {
+            return xjBleDevice;
+        }
+        if (scanRecordUtil.getManufacturerSpecificData(Constants.XJ_MANUFACTURER_ID) != null) {
+            manufacturerSpecificData = scanRecordUtil.getManufacturerSpecificData(XJ_MANUFACTURER_ID);
+            platform = XJBleDevice.PLATFORM_XJ;
+            xjBleDevice.setPlatform(XJBleDevice.PLATFORM_XJ);
+        } else if (scanRecordUtil.getManufacturerSpecificData(AL_MANUFACTURER_ID) != null) {
+            manufacturerSpecificData = scanRecordUtil.getManufacturerSpecificData(Constants.AL_MANUFACTURER_ID);
+            platform = XJBleDevice.PLATFORM_AL;
+            xjBleDevice.setPlatform(XJBleDevice.PLATFORM_AL);
+        }
+        if (manufacturerSpecificData == null) {
+            BleLog.w("get manufacturerSpecificData null!");
+            return xjBleDevice;
+        }
+        if (manufacturerSpecificData.length != 12) {
+            BleLog.w("manufacturerSpecificData length error= " + manufacturerSpecificData.length);
+            return xjBleDevice;
+        }
+        BleLog.d("get Manufacturer Specific Data: " + ByteUtils.bytesToHexString
+                (manufacturerSpecificData));
+        ManufacturerData manufacturerData =
                 new ManufacturerData(manufacturerSpecificData, platform);
-        bleDevice.setDevice(result.getDevice());
-        bleDevice.setRssi(result.getRssi());
-        bleDevice.setManufacturerData(manufacturerData);
-        return bleDevice;
+        xjBleDevice.setDevice(device);
+        xjBleDevice.setRssi(rssi);
+        xjBleDevice.setManufacturerData(manufacturerData);
+        return xjBleDevice;
     }
 
     private static final BleScanner ourInstance = new BleScanner();
 
+    public static BleScanner getInstance() {
+        return ourInstance;
+    }
 
     private BleScanner() {
     }
 
     public void scan(IBleScanCallback iBleScanCallback) {
-        bluetoothLeScanner =
-                XJBleManager.getInstance().getBluetoothAdapter().getBluetoothLeScanner();
         startLeScan(iBleScanCallback);
     }
 
     public BluetoothGatt scanAndConnect(final String mac, final IBleAuth iBleAuth,
                                         final BleConnectCallback bleConnectCallback) {
         if (TextUtils.isEmpty(mac) || iBleAuth == null || bleConnectCallback == null) {
-            Log.d("H5", "scanAndConnect null");
+            BleLog.e("scanAndConnect with empty params!");
             return null;
         }
         startLeScan(new IBleScanCallback() {
@@ -147,50 +186,43 @@ public class BleScanner {
             }
 
             @Override
-            public void onLeDeviceScanned(final XJBleDevice bleDevice) {
-                if (bleDevice == null) {
-                    Log.d("H5", "onLeDeviceScanned null");
+            public void onLeDeviceScanned(final XJBleDevice XJBleDevice) {
+                if (XJBleDevice == null) {
                     return;
                 }
-                if (mac.equals(bleDevice.getMac())) {
+                if (mac.equals(XJBleDevice.getMac())) {
                     stopScan();
-                    BleConnect.getInstance().connect(XJBleManager.getInstance().getContext(),bleDevice, bleConnectCallback);
+                    BleConnect.getInstance().connect(XJBleManager.getInstance().getContext(), XJBleDevice, bleConnectCallback);
                 }
             }
 
             @Override
-            public void onScanFinished(List<XJBleDevice> bleDeviceList) {
+            public void onScanFinished(List<XJBleDevice> XJBleDeviceList) {
 
             }
 
             @Override
             public void onScanFailed(int errorCode) {
-                Log.d("H5", "onScanFailed");
+
             }
         });
         return null;
     }
 
     public void stopScan() {
-        if (iBleScanCallback != null && mBleScanState == BleScanState.STATE_SCANNING) {
-            mBleScanState = BleScanState.STATE_IDLE;
-            Log.d("H5", "stopScan");
-            iBleScanCallback = null;
-            try {
-                bluetoothLeScanner.stopScan(scanCallback);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+        bluetoothLeScanner.stopScan(scanCallback);
+        mBleScanState = BleScanState.STATE_IDLE;
     }
 
     public void startLeScan(IBleScanCallback iBleScanCallback) {
+        bluetoothLeScanner =
+                XJBleManager.getInstance().getBluetoothAdapter().getBluetoothLeScanner();
         if (iBleScanCallback == null) {
             throw new IllegalArgumentException("can not start le scan with callback null!");
         }
         this.iBleScanCallback = iBleScanCallback;
         if (mBleScanState != BleScanState.STATE_IDLE) {
-            Log.d("H5", "scan action already exists, complete the previous scan action first");
+            BleLog.w("scan action already exists, complete the previous scan action first");
             iBleScanCallback.onScanStarted(false);
             return;
         }
@@ -204,8 +236,8 @@ public class BleScanner {
         scanFilters.add(XJScanFilterBuilder.build());
         scanFilters.add(ALScanFilterBuilder.build());
         bluetoothLeScanner.startScan(scanFilters, new ScanSettings.Builder().build(), scanCallback);
-//        mBleScanState = success ? BleScanState.STATE_SCANNING : BleScanState.STATE_IDLE;
         mBleScanState = BleScanState.STATE_SCANNING;
+        BleLog.d("start le scan……");
     }
 
 
@@ -222,18 +254,19 @@ public class BleScanner {
 //    /**
 //     * connect a known device
 //     *
-//     * @param bleDevice
+//     * @param XJBleDevice
 //     * @param bleConnectCallback
 //     * @return
 //     */
-//    public BluetoothGatt connect(BleDevice bleDevice, IBleAuth iBleAuth, BleConnectCallback bleConnectCallback) {
+//    public BluetoothGatt connect(XJBleDevice XJBleDevice, IBleAuth iBleAuth, BleConnectCallback
+//            bleConnectCallback) {
 //        if (bleConnectCallback == null) {
 //            throw new IllegalArgumentException("BleGattCallback can not be Null!");
 //        }
 //
 //        if (!XJBleManager.getInstance().isBleEnable()) {
 //            BleLog.e("Bluetooth not enable!");
-//            bleConnectCallback.onConnectFail(bleDevice, new OtherException("Bluetooth not enable!"));
+//            bleConnectCallback.onConnectFail(XJBleDevice, new OtherException("Bluetooth not enable!"));
 //            return null;
 //        }
 //
@@ -241,11 +274,11 @@ public class BleScanner {
 //            BleLog.w("Be careful: currentThread is not MainThread!");
 //        }
 //
-//        if (bleDevice == null || bleDevice.getDevice() == null) {
-//            bleConnectCallback.onConnectFail(bleDevice, new OtherException("Not Found Device Exception Occurred!"));
+//        if (XJBleDevice == null || XJBleDevice.getDevice() == null) {
+//            bleConnectCallback.onConnectFail(XJBleDevice, new OtherException("Not Found Device Exception Occurred!"));
 //        } else {
-////            BleConnect bleConnect = XJBleManager.getInstance().getMultipleBluetoothController().buildConnectingBle(bleDevice);
-//            return bleConnect.connect(bleDevice, iBleAuth, bleConnectCallback);
+//            BleConnect bleConnect = XJBleManager.getInstance().getMultipleBluetoothController().buildConnectingBle();
+//            return bleConnect.connect(XJBleDevice, iBleAuth, bleConnectCallback);
 //        }
 //
 //        return null;
